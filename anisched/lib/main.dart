0207af8e72552002d81f4e683e15643116628a9d
Novang <qkdxorjs1002@naver.com>
2021-03-25 19:35:52 +0900

    Widget: Implement home page

--- anisched/lib/main.dart
+++ anisched/lib/main.dart
@@ -1,68 +1,21 @@
-import 'package:anisched/repository/anissia/service.dart';
 import 'package:flutter/material.dart';
 
+import 'ui/page/home.dart';
+
 void main() {
-    runApp(MyApp());
+    runApp(App());
 }
 
-class MyApp extends StatelessWidget {
+class App extends StatelessWidget {
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
-            title: 'Flutter Demo',
+            color: Colors.black,
             theme: ThemeData(
-                primarySwatch: Colors.blue,
+                brightness: Brightness.dark,
+                primarySwatch: Colors.indigo,
             ),
-            home: MyHomePage(title: 'Flutter Demo Home Page'),
-        );
-    }
-}
-
-class MyHomePage extends StatefulWidget {
-    MyHomePage({Key key, this.title}) : super(key: key);
-
-    final String title;
-
-    @override
-    _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-    int _counter = 0;
-
-    void _incrementCounter() {
-        setState(() {
-            _counter++;
-            final api = AnissiaService();
-            api.requestSchedule(1).then((value) => print(value));
-        });
-    }
-
-    @override
-    Widget build(BuildContext context) {
-        return Scaffold(
-            appBar: AppBar(
-                title: Text(widget.title),
-            ),
-            body: Center(
-                child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                    Text(
-                        'You have pushed the button this many times:',
-                    ),
-                    Text(
-                        '$_counter',
-                        style: Theme.of(context).textTheme.headline4,
-                    ),
-                ],
-                ),
-            ),
-            floatingActionButton: FloatingActionButton(
-                onPressed: _incrementCounter,
-                tooltip: 'Increment',
-                child: Icon(Icons.add),
-            ),
+            home: HomePage(week: DateTime.now().weekday),
         );
     }
 }
---
+++ anisched/lib/ui/page/home.dart
@@ -0,0 +1,34 @@
+import 'package:anisched/repository/anissia/model.dart';
+import 'package:anisched/ui/widget/rank.dart';
+import 'package:anisched/ui/widget/schedule.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+    final int week;
+    
+    const HomePage({ Key key, this.week }) : super(key: key);
+
+    @override
+    Widget build(BuildContext context) {
+        return Scaffold(
+            backgroundColor: Colors.black,
+            body: Row(
+                children: [
+                    Expanded(
+                        child: Schedule(
+                            week: week,
+                        ),
+                    ),
+                    SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.35,
+                        height: double.infinity,
+                        child: Ranking(
+                            factor: FACTOR.WEEK,
+                        ),
+                    ),
+                ],
+            ),
+        );
+    }
+}
