1e296933a60fd36827d522f799ff9cae752fe566
Novang <qkdxorjs1002@naver.com>
2021-03-30 04:44:55 +0900

    Implement ObservableData

---
+++ anisched/lib/arch/observable.dart
@@ -0,0 +1,46 @@
+class ObservableData<T> {
+
+    T _data;
+
+    List<Observer> _observerList = [];
+
+    ObservableData({ T data }) {
+        _data = data;
+    }
+
+    T get getData {
+        return _data;
+    }
+
+    void setData(T data) {
+        _data = data;
+
+        Iterator iterator = _observerList.iterator;
+        while (iterator.moveNext()) {
+            iterator?.current?.call(_data);
+        }
+    }
+
+    void addObserver(Observer<T> observer) {
+        _observerList.add(observer);
+    }
+
+    void removeAllObservers() {
+        _observerList = [];
+    }
+
+}
+
+class Observer<T> {
+
+    final _onObservableDataChanged;
+
+    Observer(this._onObservableDataChanged);
+
+    void call(T data) {
+        Future(() {
+            _onObservableDataChanged(data);
+        });
+    }
+
+}
