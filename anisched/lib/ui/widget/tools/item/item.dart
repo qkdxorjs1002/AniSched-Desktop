2d19701bda7b45a7d7c87aeec0a53b827fb7977f
paragonnov <qkdxorjs1002@naver.com>
2021-07-27 18:23:44 +0900

    Widget: Add Tools widget

---
+++ anisched/lib/ui/widget/tools/item/item.dart
@@ -0,0 +1,52 @@
+import 'package:anisched/ui/widget/sizes.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class ToolsItem extends StatelessWidget {
+
+    final Widget icon;
+    final String text;
+    final Function onItemClick;
+
+    ToolsItem({ @required this.icon, @required this.text, this.onItemClick });
+
+    @override
+    Widget build(BuildContext context) {
+        return Container(
+            color: Theme.of(context).primaryColor.withOpacity(0.1),
+            child: Material(
+                color: Colors.transparent,
+                child: InkWell(
+                    onTap: () {
+                        if (onItemClick != null) {
+                            onItemClick();
+                        }
+                    },
+                    child: Padding(
+                        padding: EdgeInsets.fromLTRB(Sizes.SIZE_020, Sizes.SIZE_010, Sizes.SIZE_020, Sizes.SIZE_010),
+                        child: Row(
+                            children: [
+                                Align(
+                                    alignment: Alignment.center,
+                                    child: icon,
+                                ),
+                                Align(
+                                    alignment: Alignment.centerLeft,
+                                    child: Text(
+                                        text,
+                                        style: TextStyle(
+                                            fontSize: Sizes.SIZE_014,
+                                            fontWeight: FontWeight.w500,
+                                        ),
+                                        maxLines: 1,
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
+                ),
+            ),
+        );
+    }
+
+}
