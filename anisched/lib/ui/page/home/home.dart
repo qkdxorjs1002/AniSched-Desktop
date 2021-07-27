94bf18f2327150dd0b8ff2bb2739a78cade7ff97
paragonnov <qkdxorjs1002@naver.com>
2021-07-27 18:25:09 +0900

    HomePage: Add Search, ClearCache tools and footer

--- anisched/lib/ui/page/home/home.dart
+++ anisched/lib/ui/page/home/home.dart
@@ -2,14 +2,17 @@ import 'package:anisched/repository/anissia/model.dart';
 import 'package:anisched/repository/tmdb/model.dart';
 import 'package:anisched/helper.dart';
 import 'package:anisched/ui/page/detail/detail.dart';
-import 'package:anisched/ui/widget/blur.dart';
+import 'package:anisched/ui/page/search/search.dart';
 import 'package:anisched/ui/widget/board.dart';
 import 'package:anisched/ui/widget/ranking/ranking.dart';
 import 'package:anisched/ui/widget/recent/recent.dart';
 import 'package:anisched/ui/widget/sizes.dart';
-import 'package:anisched/ui/widget/timetable/table.dart';
+import 'package:anisched/ui/widget/timetable/timetable.dart';
+import 'package:anisched/ui/widget/tools/item/item.dart';
+import 'package:anisched/ui/widget/tools/tools.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_cache_manager/flutter_cache_manager.dart';
 
 class HomePage extends StatelessWidget {
     
@@ -29,6 +32,60 @@ class HomePage extends StatelessWidget {
                     Ranking(
                         onItemClick: (Anime anime, Result tmdb) => Helper.navigateRoute(context, DetailPage(animeId: anime.id)),
                     ),
+                    Padding(
+                        padding: EdgeInsets.only(top: Sizes.SIZE_020),
+                        child: Tools(
+                            children: [
+                                ToolsItem(
+                                    icon: Icon(
+                                        Icons.search,
+                                        size: Sizes.SIZE_024,
+                                    ), 
+                                    text: "",
+                                    onItemClick: () => Helper.navigateRoute(context, SearchPage()),
+                                ),
+                                ToolsItem(
+                                    icon: Icon(
+                                        Icons.hide_image_outlined,
+                                        size: Sizes.SIZE_024,
+                                    ), 
+                                    text: "",
+                                    onItemClick: () => ScaffoldMessenger.of(context).showSnackBar(
+                                        SnackBar(
+                                            backgroundColor: Theme.of(context).backgroundColor,
+                                            content: Text(
+                                                "이미지 캐시를 삭제합니다. 이미지 캐시를 제거할 경우, 일시적으로 데이터 사용량이 증가할 수 있습니다.",
+                                                style: TextStyle(
+                                                    color: Theme.of(context).primaryColor,
+                                                    fontWeight: FontWeight.w300,
+                                                ),
+                                            ),
+                                            action: SnackBarAction(
+                                                textColor: Theme.of(context).primaryColor,
+                                                disabledTextColor: Theme.of(context).primaryColor.withOpacity(0.5),
+                                                label: "캐시 삭제",
+                                                onPressed: () {
+                                                    DefaultCacheManager().emptyCache();
+                                                    ScaffoldMessenger.of(context).showSnackBar(
+                                                        SnackBar(
+                                                            backgroundColor: Theme.of(context).backgroundColor,
+                                                            content: Text(
+                                                                "캐시를 제거했습니다.",
+                                                                style: TextStyle(
+                                                                    color: Theme.of(context).primaryColor,
+                                                                    fontWeight: FontWeight.w300,
+                                                                ),
+                                                            ),
+                                                        ),
+                                                    );
+                                                },
+                                            ),
+                                        ),
+                                    ),
+                                ),
+                            ],
+                        ),
+                    ),
                     Board(
                         title: "최근 자막",
                         description: "길게 누르면 작품 정보",
@@ -47,22 +104,22 @@ class HomePage extends StatelessWidget {
                             onItemClick: (anime, tmdb) => Helper.navigateRoute(context, DetailPage(animeId: anime.id)),
                         ),
                     );
-                }),
-            ),
-        );
-    }
-
-    void _navigateRoute(BuildContext context, int id) {
-        Navigator.of(context).push(
-            PageRouteBuilder(
-                opaque: false,
-                transitionDuration: const Duration(milliseconds: 500),
-                pageBuilder: (context, animation, secondaryAnimation) => BlurTransition(
-                    animation: Tween<double>(begin: 30.0, end: 0.0).animate(animation),
-                    child: DetailPage(
-                        animeId: id,
+                }) + [
+                    Container(
+                        alignment: Alignment.center,
+                        child: Padding(
+                            padding: EdgeInsets.symmetric(vertical: Sizes.SIZE_030),
+                            child: Text(
+                                "©paragonnov (github.com/qkdxorjs1002) - DB from 'Anissia' and 'TMDB'",
+                                style: TextStyle(
+                                    color: Theme.of(context).primaryColor.withOpacity(0.65),
+                                    fontSize: Sizes.SIZE_010,
+                                    fontWeight: FontWeight.w300,
+                                ),
+                            ),
+                        ),
                     ),
-                ),
+                ],
             ),
         );
     }
