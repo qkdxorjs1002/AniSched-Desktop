d80676aa4dd0bdce2d1fd725caa80cc9441f3c55
paragonnov <qkdxorjs1002@naver.com>
2021-07-28 16:46:32 +0900

    Windows: Versioning

---
+++ anisched/version.bat
@@ -0,0 +1,1 @@
+powershell "(Get-Content pubspec.yaml) -replace '(version: \d.\d.\d\+\d.*)', ('version: ' + $(git describe --tags --abbrev=0) + '+' + $(git rev-list --count $(git describe --tags --abbrev=0))) | Out-File -Encoding 'UTF8' pubspec.yaml"
