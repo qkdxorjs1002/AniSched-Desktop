b1f21f403613110b92d2e220aa1c9c27db055124
paragonnov <qkdxorjs1002@naver.com>
2021-08-01 19:35:02 +0900

    Project: Missing path

--- anisched/deploy_mac.sh
+++ anisched/deploy_mac.sh
@@ -2,6 +2,6 @@ echo "Building..."
 flutter clean && flutter build macos --release --pub --build-name=$(git describe --tags --abbrev=0) --build-number $(git rev-list --count $(git describe --tags --abbrev=0))
 
 echo "Creating DMG..."
-create-dmg --identity="Apple Development: qkdxorjs1002@naver.com (6CAP6ZVWNS)" --overwrite ./build/macos/Build/Release/AniSched.app ./build/macos/Build/Release
+create-dmg --identity="Apple Development: qkdxorjs1002@naver.com (6CAP6ZVWNS)" --overwrite ./build/macos/Build/Products/Release/AniSched.app ./build/macos/Build/Products/Release
 
 echo "Done."
